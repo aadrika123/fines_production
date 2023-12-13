add61f4ca0b7694456ea9833e7ec476bda1b36ac
Talib Hussain <talibplay111@gmail.com>
2023-12-13 16:50:16 +0530

    updates

---
+++ fine_penalty_build/assets/BackButton-5eb4a895.js
@@ -0,0 +1,1 @@
+import{a,j as t,O as n}from"./index-7107b133.js";const o=()=>{const e=a(),s=()=>{window.history.length==0?e("/fines"):window.history.back()};return t.jsxs(t.Fragment,{children:[t.jsx("button",{className:"relative top-2 left-2 px-4 py-1 bg-slate-500 text-sm text-white hover:bg-slate-600",onClick:()=>s(),children:"Back"}),t.jsx(n,{})]})};export{o as default};
--- fine_penalty_build/assets/useSetTitle-934bec7c.js
+++
@@ -1,1 +0,0 @@
-import{R as a,c as o,r}from"./index-61b740df.js";function n(t,e){const{settitleText:s,settitleBarVisibility:i}=a.useContext(o);r.useEffect(()=>{s(t),i(e)},[])}export{n as u};
