dbbbbd213a8d277bd1133f6f6e065a7a4669ef4d
Talib Hussain <talibplay111@gmail.com>
2023-12-13 16:20:11 +0530

    updates

--- fine_penalty_build/assets/useSetTitle-1a58b40d.js
+++
@@ -1,1 +0,0 @@
-import{R as a,c as o,r}from"./index-39173d9b.js";function n(t,e){const{settitleText:s,settitleBarVisibility:i}=a.useContext(o);r.useEffect(()=>{s(t),i(e)},[])}export{n as u};
---
+++ fine_penalty_build/assets/useSetTitle-934bec7c.js
@@ -0,0 +1,1 @@
+import{R as a,c as o,r}from"./index-61b740df.js";function n(t,e){const{settitleText:s,settitleBarVisibility:i}=a.useContext(o);r.useEffect(()=>{s(t),i(e)},[])}export{n as u};
